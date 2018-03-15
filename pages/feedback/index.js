5c7847f9a4ddc861dbaef9479b0f8bdb1a0348c0
zhengfei <zhengfei@zillionfortune.com>
2018-03-15 16:14:14 +0800

    测试

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,70 +1,51 @@
-// pages/feedback/index.js
+var feedbackApi = require('../../utils/feedbackApi');
+
 Page({
   data: {
-    src: './images/feedback_img.png'
+    src: './images/feedback_img.png',
+
+    noteMaxLen: 200,//备注最多字数
+
+    info: "",
+
+    noteNowLen: 0,//备注当前字数
+  },
+  clickButton:function(){
+    var that = this;
+    
+    if (this.data.info==""){
+      feedbackApi.showToast({ title: '提交内容不能为空！' })//调用 
+      
+    }else{
+      console.log(this.data.info);
+      wx.showToast({
+        title: '提交成功',
+        icon: 'success',
+        duration: 1500,
+        mask: false,
+        success:function(){
+          that.setData({
+            info: "",
+
+            noteNowLen: 0
+          })
+        }
+      })
+      
+    }
+      
   },
 
+  valueChange:function(e){
+    var that = this;
+    var value = e.detail.value, len = parseInt(value.length);
+    if (len > that.data.noteMaxLen) return;
+    that.setData({
 
-  /**
-   * 页面的初始数据
-   */
+      info: value,
 
+      noteNowLen: len
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    })
+  } 
 })
