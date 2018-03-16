b8f9db6a06289ce2d7ce58d537f10db8c716883c
zhengfei <zhengfei@zillionfortune.com>
2018-03-16 18:28:23 +0800

    我的订单开发

--- pages/order/myorder.js
+++ pages/order/myorder.js
@@ -1,8 +1,17 @@
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    navbar: ['进行中', '已完成', '已取消'],
+    currentTab: 0  ,
+    orderlist: ["订单号：111", "订单号：112", "订单号：113","订单号：114","订单号：115","订单号：116","订单号：117","订单号：118","订单号：119"],
+    orderlist1:"",
+    orderlist2:""
+  },
+  navbarTap: function (e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
   }})
+  
