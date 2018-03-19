736e71b90a6874d2c5db8cd851730404bef5a937
zhengfei <zhengfei@zillionfortune.com>
2018-03-19 17:53:11 +0800

    首页开发

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,21 +1,31 @@
 // pages/order/order.js
+var util = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isDis:'no-display'
-  },
-  bindDateChange: function (e) {
-    this.setData({
-      date: e.detail.value
-    })
-  },
-  bindTimeChange: function (e) {
-    this.setData({
-      time: e.detail.value
-    })
+    btntext:"立即回收",
+    isouttime:false,
+    multiIndex:0,
+    inputtext:"",
+    isDis:'no-display',
+    listTime1:[],
+    listData: [{
+      id: 0,
+      name: util.getDateStr(false, null) + "(今天)"
+    },
+    {
+      id: 1,
+      name: util.getDateStr(false, 1) + "(明天)"
+    },
+    {
+      id: 2,
+      name: util.getDateStr(false, 2) + "(后天)"
+    }],
+    listTime: [{ id: 0, name: "9:00-11:00" }, { id: 1, name: "11:00-13:00" }, { id: 2, name: "13:00-15:00" }, { id: 3, name: "15:00-17:00" }, { id: 4, name: "17:00-19:00" }],
+    objectMultiArray: []
   },
   /**
    * 获取地图地址返回到页面上
@@ -37,22 +47,135 @@ Page({
    * 点击预约出现 时间选择
    */
   bespeak:function(e) {
-    this.setData({
-      isDis:'display'
-    })
+    var that = this;
+    if (that.data.isDis =="display"){
+      this.setData({
+        isDis: 'no-display',
+        inputtext:"",
+        btntext:"立即回收"
+      })
+    }else{
+      this.setData({
+        isDis: 'display',
+        inputtext: "",
+        btntext: "预约回收"
+      })
+    }
+    
+  },
+  formSubmit:function(e){
+    var that = this;
+    if (that.checkInput(e)){
+      wx.showToast({
+        title: "提交成功",
+        icon: 'none',
+        duration: 1500
+      })
+    }
+  },
+  checkInput(e){
+    var that = this;
+    var data = e.detail.value;
+    var name = data.name;
+    var phoneNo = data.phoneNo;
+    var address = data.address;
+    var adressDetila = data.adressDetila;
+    var notes = data.notes;
+    var time = data.time;
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+    var toastStr;
+    var isToast=true;
+    if (name.length==0){
+      toastStr="联系人不能为空"
+    } else if (phoneNo.length==0){
+      toastStr = "手机号不能为空"
+    } else if (address.length == 0) {
+      toastStr = "收货地址不能为空"
+    } else if (adressDetila.length == 0) {
+      toastStr = "详细地址不能为空"
+    } else if (phoneNo.length != 11){
+      toastStr = "请输入正确的手机号！"
+    } else if (!myreg.test(phoneNo)) {
+      toastStr = "请输入正确的手机号！"
+    }else{ 
+      isToast=false;
+    }
+    if(isToast){
+      wx.showToast({
+        title: toastStr,
+        icon: 'none',
+        duration: 1500
+      })
+      return false;
+    }else{
+      if (this.data.isouttime && time.length==0){
+        wx.showToast({
+          title: "今天上门回收时间已过，请预约明天上门",
+          icon: 'none',
+          duration: 1500
+        })
+        return false;
+      }
+      return true;
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
     wx.setNavigationBarTitle({ title: '订单详情' });
-    var is_dis = false;
-    if(is_dis) {
-      
+    var hours = util.getHours();
+    var list2 ;
+    if(hours<9){
+      list2 = that.data.listTime;
+    }else if(hours<11){
+      list2 = that.data.listTime.slice(1);
+    }else if(hours<13){
+      list2 = that.data.listTime.slice(2);
+    }else if(hours<15){
+      list2 = that.data.listTime.slice(3);
+    }else if(hours<17){
+      list2 = that.data.listTime.slice(4);
+    }else{
+      list2 = that.data.listTime.slice(5);
+      that.data.isouttime=true;
     }
+    var list1 = that.data.listData;
+    that.data.listTime1 = list2;
+    this.setData({
+      objectMultiArray: [list1, list2]
+    })
   },
+  bindMultiPickerChange2:function(e){
+    var that = this;
+    var inxex1 = e.detail.value[0];
+    var index2 = e.detail.value[1];
+    var str = this.data.objectMultiArray[0][inxex1].name +" — "+ this.data.objectMultiArray[1][index2].name;
+    this.setData({
+      inputtext:str,
+    })
 
+  },
+  bindMultiPickerColumnChange2:function(e){
+    var that = this;
+    var list1 = that.data.listData;
+    var list2;
+    var value= e.detail.value;
+    if (e.detail.column==0){
+      if(value==0){
+        list2 = that.data.listTime1;
+      }else{
+        list2 = that.data.listTime;
+      }
+      this.setData({
+        objectMultiArray: [list1, list2]
+      })
+    }
+    
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -64,7 +187,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+   
+    
   },
 
   /**
--- utils/util.js
+++ utils/util.js
@@ -15,5 +15,32 @@ const formatNumber = n => {
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getDateStr: getDateStr,
+  getHours:getHours
+}
+
+function getDateStr(today, addDayCount) {
+  var dd;
+  if (today) {
+    dd = new Date(today);
+  } else {
+    dd = new Date();
+  }
+  dd.setDate(dd.getDate() + addDayCount);//获取AddDayCount天后的日期 
+  var y = dd.getFullYear();
+  var m = dd.getMonth() + 1;//获取当前月份的日期 
+  var d = dd.getDate();
+  if (m < 10) {
+    m = '0' + m;
+  };
+  if (d < 10) {
+    d = '0' + d;
+  };
+  return  m + "-" + d;
+}
+
+function getHours(){
+  var dd = new Date();
+  return dd.getHours();
 }
