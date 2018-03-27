0dfc9771cb146c4f8b50e7d6ee91381b2433853d
zhengfei <zhengfei@zillionfortune.com>
2018-03-27 17:19:56 +0800

    登录验证码网路请求

--- pages/sign-in/index.js
+++ pages/sign-in/index.js
@@ -1,4 +1,4 @@
-﻿// pages/me/index.js
+// pages/me/index.js
 Page({
   data: {
     src: './images/logo.jpg',
@@ -10,7 +10,8 @@ Page({
     codeText: "获取验证码",
     btnClickable: false,
     isClickable1: false,
-    phoneNo: ''
+    phoneNo: '',
+    isClickable2:false
   },
 
 
@@ -24,137 +25,122 @@ Page({
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({ title: '登录' });
-    
+
   },
   formSubmit: function (e) {
     if (!this.data.btnClickable) {
       return;
     }
-    var that = this;
-    var data = e.detail.value
-    wx.showToast({
-      title: '登录成功',
-      icon: 'success',
-      duration: 1500
-    })
-    wx.switchTab({
-      url: '../../pages/order/order'
-    });
-    wx.request({
-      url: 'http://www.lazyfei.top/api/user/check-login',
-      data: {
-        phone: data.phone,
-        vcode: data.vcode
-      },
-      header: { 'content-type': 'application/x-www-form-urlencoded' },
-      method: 'POST',
-      success: function (res) {
-        console.log(res.data);
-      } 
-    });
-
-  formSubmit:function(e) {
     wx.login({
-      success:function(res) {
-        if(res.code) {
+      success: function (res) {
+        if (res.code) {
           wx.request({
-            'url':'http://www.lazyfei.top/api/login/to-login', // 登录接口
-            data:{
+            'url': 'http://www.lazyfei.top/api/login/to-login', // 登录接口
+            data: {
               code: res.code, // 必须传递
               phone: '17621861270', // 手机号
               vcode: '666'  // y验证码
             },
             header: { 'content-type': 'application/x-www-form-urlencoded' },
             method: 'POST',
-            success:function(res) {
+            success: function (res) {
+              console.log(res.data);
               var key = res.data.key;
               // 这里我的缓存是测试，用的是同步，你之后写的用异步
               wx.setStorageSync('acc_key', key); // 成功写入缓存
-              var key = wx.getStorageSync('acc_key'); // 获取缓存的数据
-              /**
-               * 以下是测试登录态
-               * 不管你发送什么请求，都要带上这个参数 key 
-               */
-              wx.request({
-                url: 'http://www.lazyfei.top/api/login/test',
-                data:{
-                  key : key  // 参数key 从缓存中获取到
-                },
-                header: { 'content-type': 'application/x-www-form-urlencoded' },
-                method: 'POST',
-                success: function(res) {
-
-                }
+              if (res.data.code == 200) {
+                wx.showToast({
+                  title: '登录成功',
+                  icon: 'success',
+                  duration: 1500
+                })
+                wx.switchTab({
+                  url: '../../pages/order/order'
+                });
+              } else {
+                wx.showToast({
+                  title: res.data.msg,
+                  icon: 'success',
+                  duration: 1500
+                })
+              }
+            },
+            fail:function(e){
+              wx.showToast({
+                title: "网络不好请重试~",
+                icon: 'success',
+                duration: 1500
               })
             }
+
           })
         }
       }
     })
-    // var that = this;
-    // var data = e.detail.value;
-    // var phone = data.phone;
-    // var vcode = data.vcode;
-    // var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
-    // if(phone.length == 0) {
-    //   wx.showToast({
-    //     title: '请输入手机号！',
-    //     icon:'none',
-    //     duration: 1500
-    //   })
-    //   return false;
-    // }else if (phone.length != 11) {
-    //     wx.showToast({
-    //       title: '手机号长度有误！',
-    //       icon: 'none',
-    //       duration: 1500
-    //     })
-    //     return false;
-    // } else if (!myreg.test(phone)) {
-    //     wx.showToast({
-    //       title: '手机号有误！',
-    //       icon: 'none',
-    //       duration: 1500
-    //     })
-    //     return false;
-    // } else if (vcode.length == 0) {
-    //   wx.showToast({
-    //     title: '请输入验证码！',
-    //     icon: 'none',
-    //     duration: 1500
-    //   })
-    //   return false;
-    //   }else {
-    //     wx.request({
-    //       url: 'http://www.lazyfei.top/api/user/check-login',
-    //       data: data,
-    //       header: { 'content-type': 'application/x-www-form-urlencoded' },
-    //       method: 'POST',
-    //       success: function (res) {
-    //         var data = res.data;
-    //         that.setData({
-    //           isLogin: true
-    //         });
-    //         if (data.code == 200) {
-    //           var id = data.id;
-    //           wx.setStorage({
-    //             key: "id",
-    //             data: id
-    //           });
-    //           var isLogin = that.data.isLogin;
-    //           wx.showToast({
-    //             title: '登录成功',
-    //             icon: 'success',
-    //             duration: 1500
-    //           })
-    //           wx.reLaunch({
-    //             url: "/pages/personal-center/index?isLogin=" + isLogin
-    //           })
-    //         }
-    //       }
-    //     })
-    //   }
 
+    var that = this;
+    var data = e.detail.value;
+    var phone = data.phone;
+    var vcode = data.vcode;
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+    if (phone.length == 0) {
+      wx.showToast({
+        title: '请输入手机号！',
+        icon: 'none',
+        duration: 1500
+      })
+      return false;
+    } else if (phone.length != 11) {
+      wx.showToast({
+        title: '手机号长度有误！',
+        icon: 'none',
+        duration: 1500
+      })
+      return false;
+    } else if (!myreg.test(phone)) {
+      wx.showToast({
+        title: '手机号有误！',
+        icon: 'none',
+        duration: 1500
+      })
+      return false;
+    } else if (vcode.length == 0) {
+      wx.showToast({
+        title: '请输入验证码！',
+        icon: 'none',
+        duration: 1500
+      })
+      return false;
+    } else {
+      wx.request({
+        url: 'http://www.lazyfei.top/api/user/check-login',
+        data: data,
+        header: { 'content-type': 'application/x-www-form-urlencoded' },
+        method: 'POST',
+        success: function (res) {
+          var data = res.data;
+          that.setData({
+            isLogin: true
+          });
+          if (data.code == 200) {
+            var id = data.id;
+            wx.setStorage({
+              key: "id",
+              data: id
+            });
+            var isLogin = that.data.isLogin;
+            wx.showToast({
+              title: '登录成功',
+              icon: 'success',
+              duration: 1500
+            })
+            wx.reLaunch({
+              url: "/pages/personal-center/index?isLogin=" + isLogin
+            })
+          }
+        }
+      })
+    }
   },
 
 
@@ -249,7 +235,7 @@ Page({
         isClickable: true,
         phoneNo: value
       })
-    } else {
+    } else if (that.isClickable){
       that.setData({
         textColor: '#b9b9b9',
         isClickable: false
@@ -273,9 +259,40 @@ Page({
     var that = this;
     if (that.data.isClickable) {
       var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
-      console.log("aa" + that.data.phoneNo);
       if (myreg.test(that.data.phoneNo)) {
-        that.countdown();
+        wx.request({
+          'url': '', // 获取验证码接口
+          data: {
+            phone: '17621861270', // 手机号
+          },
+          header: { 'content-type': 'application/x-www-form-urlencoded' },
+          method: 'POST',
+          success: function (res) {
+            console.log(res.data);
+            if (res.data.code == 200) {
+              wx.showToast({
+                title: '获取验证码成功',
+                icon: 'success',
+                duration: 1500
+              })
+              that.countdown();
+            } else {
+              wx.showToast({
+                title: res.data.msg,
+                icon: 'success',
+                duration: 1500
+              })
+            }
+          },
+          fail: function (e) {
+            wx.showToast({
+              title: "网络不好请重试~",
+              icon: 'success',
+              duration: 1500
+            })
+          }
+
+        })       
       } else {
         wx.showToast({
           title: '手机号有误！',
@@ -303,15 +320,24 @@ Page({
     var interval = setInterval(function () {
       if (count < 0) {
         clearInterval(interval);
-        that.setData({
-          textColor: '#F27430',
-          codeText: '重新获取',
-          isClickable: true
-        })
+        if (!that.data.isClickable){
+          that.setData({
+            textColor: '#b9b9b9',
+            codeText: '重新获取',
+            isClickable2: true
+          })
+        }else{
+          that.setData({
+            textColor: '#F27430',
+            codeText: '重新获取',
+            isClickable2: true
+          })
+        }
+        
       } else {
         that.setData({
           textColor: '#333',
-          isClickable: false,
+          isClickable2: false,
           codeText: count + "s"
         })
       }
